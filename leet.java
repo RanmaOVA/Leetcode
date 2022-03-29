0c88ad9efd65e68eb07fbbf4a52f21dc88352193
RanmaOVA <Masseymichael643@gmail.com>
2022-03-29 07:30:36 -0500

    sort

--- leet.java
+++ leet.java
@@ -30,3 +30,32 @@ class Solution {
         return t1;
     }
 }
+
+////////////////////////////////////////////////////////////////
+
+//   Sort Array By Parity
+
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        
+        int[] new_arr = new int[nums.length];
+        int counter = 0;
+        
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                new_arr[counter] = nums[i];
+                counter++;
+            }
+        }
+        
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] % 2 == 1) {
+                new_arr[counter] = nums[i];
+                counter++;
+            }
+        }
+        
+        return new_arr;
+        
+    }
+}
